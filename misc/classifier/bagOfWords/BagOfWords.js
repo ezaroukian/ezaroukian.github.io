2b14eb001e29fedf13b8062861854b0b4d3ca9ab
ezaroukian <egz@jhu.edu>
2015-08-04 17:24:24 -0400

    Add table borders

--- misc/classifier/bagOfWords/BagOfWords.js
+++ misc/classifier/bagOfWords/BagOfWords.js
@@ -133,7 +133,7 @@ function categorize(input, option1, option2){
 	winnerString += "<p>Biggest cosine / smallest angle wins<p>";
 	
 	var counter = 0;
-	winnerString +="<table style='border:1px solid black;border-collapsecollapse;'>";//add borders to hd, td...
+	winnerString +="<table style='border:1px solid black;border-collapse:collapse;'>";//add borders to hd, td...
 	winnerString +="<tr><th>Word</th><th>Kirk</th><th>you</th><th>Spock</th></tr>";
 	for (var word in allWords){
 		winnerString +="<tr style='border:1px solid black;border-collapse:collapse;'>";
