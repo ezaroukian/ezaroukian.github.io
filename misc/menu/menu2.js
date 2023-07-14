2a0a9db4cdf4ea4416fe7ae71c10bd2ffbee4282
ezaroukian <ezaroukian@gmail.com>
2023-07-14 19:24:18 -0400

    Add menu

---
+++ misc/menu/menu2.js
@@ -0,0 +1,78 @@
+var today = new Date();
+//var today = new Date(2023, 6, 22);//testing
+var d = String(today.getDate());
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var month = months[today.getMonth()];
+
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var day = days[today.getDay()];
+
+
+
+//should get date in Eastern time
+//week 1 starts 7/10/2023 -- moved to Saturday, hopefully this means the next week will show up on the weekend.
+week1 = new Date(2023, 6, 8);//month is 0 indexed, = Saturday July 8 
+week = (1 + Math.floor((today.getDate() - week1.getDate()) / 7)) % 4;
+
+
+//document.getElementById("test").innerHTML = today;
+document.getElementById("todayIs").innerHTML = "Today is "+day+", "+month+" "+d+", "+yyyy+". Consult Week "+week+" for the next meal.";
+
+
+window.onload = function () {
+    switch (week) {
+        case 1:
+            var elements = document.getElementsByClassName("first");
+            var non = document.querySelectorAll('.second,.third,.fourth');
+            break;
+        case 2:
+            var elements = document.getElementsByClassName("second");
+            var non = document.querySelectorAll('.first,.third,.fourth');
+            break;
+        case 3:
+            var elements = document.getElementsByClassName("third");
+            var non = document.querySelectorAll('.first,.second,.fourth');
+            break;
+        case 4:
+            var elements = document.getElementsByClassName("fourth");
+            var non = document.querySelectorAll('.first,.second,.third');
+            break;
+        default:
+            var elements = [];
+            var non = [];
+    }
+    switch (day) {
+        case "Monday":
+            var colElements = document.getElementsByClassName("m");
+            break;
+        case "Tuesday":
+            var colElements = document.getElementsByClassName("t");
+            break;
+        case "Wednesday":
+            var colElements = document.getElementsByClassName("w");
+            break;
+        case "Thursday":
+            var colElements = document.getElementsByClassName("h");
+            break;
+        case "Friday":
+            var colElements = document.getElementsByClassName("f");
+            break;
+        default:
+            var colElements = [];
+    }
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('highlight');
+    }
+    for (var i = 0; i < colElements.length; i++) {
+        colElements[i].classList.add('daylight');
+    }
+    for (var i = 0; i < non.length; i++) {
+        non[i].classList.add('dim');
+    }
+
+   
+}
